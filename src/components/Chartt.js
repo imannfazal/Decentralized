c4eaaf832e6ec45340013111d5509f3a1031cd22
Kamal Farooqi <76092669+kamalaqeel@users.noreply.github.com>
2022-11-11 20:53:16 +0500

    push it

--- src/components/Chartt.js
+++ src/components/Chartt.js
@@ -3,15 +3,15 @@ import { Line } from '@ant-design/charts';
 
 const Chartt = () => {
   const data = [
-    { year: '11/1/2022', value: 3 },
-    { year: '11/2/2022', value: 4 },
-    { year: '11/3/2022', value: 3.5 },
-    { year: '11/4/2022', value: 5 },
-    { year: '11/5/2022', value: 4.9 },
-    { year: '11/6/2022', value: 6 },
-    { year: '11/7/2022', value: 7 },
-    { year: '11/8/2022', value: 9 },
-    { year: '11/9/2022', value: 13 },
+    { year: '11/1/22', value: 3 },
+    { year: '11/2/22', value: 4 },
+    { year: '11/3/22', value: 3.5 },
+    { year: '11/4/22', value: 5 },
+    { year: '11/5/22', value: 4.9 },
+    { year: '11/6/22', value: 6 },
+    { year: '11/7/22', value: 7 },
+    { year: '11/8/22', value: 9 },
+    { year: '11/9/22', value: 13 },
   ];
 
   const config = {
